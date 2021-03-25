46d8a5fa9ced48a5370737f11d589714b3daac39
ascyrax <ascyrax01@gmail.com>
2021-03-25 08:25:40 +0530

    Solved the second exercise named as repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+	let ans="";
+	if(num==-1) return 'ERROR';
+	for(let i=0;i<num;i++){
+	    ans+=word;
+	}
+	return ans;	
 }
 
 module.exports = repeatString
