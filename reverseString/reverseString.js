40869dd794f59cddd1365088fcb181ffee8ce7b1
ascyrax <ascyrax01@gmail.com>
2021-03-25 08:33:46 +0530

    solved reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+let ans="";
+let wlen=word.length;
+for(let i=wlen-1;i>=0;i--){
+  ans+=word[i];
+}
+return ans;
 }
 
 module.exports = reverseString
