8e96eeaa3f0d7b5acade0fad1ab17cc1b92ce102
ascyrax <ascyrax01@gmail.com>
2021-03-25 11:05:37 +0530

    added sumAll after solving

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+let sum =0;
+if(a<0||b<0)
+return 'ERROR';
+else if(!(Number.isInteger(a)&&Number.isInteger(b)))
+return 'ERROR';
+for(let small=Math.min(a,b);small<=Math.max(a,b);small++){
+sum+=small;
+}
+return sum;
 }
 
 module.exports = sumAll
